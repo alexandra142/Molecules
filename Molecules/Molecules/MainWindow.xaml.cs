49fd59114bfcb337b7e861ff039bb708b8fd030b
Alexandra Pistrakova <alex.pistrakova@gmail.com>
2017-07-08 14:41:28 +0200

    Canvas created, constants set

--- Molecules/Molecules/MainWindow.xaml.cs
+++ Molecules/Molecules/MainWindow.xaml.cs
@@ -20,9 +20,30 @@ namespace Molecules
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region constants
+        private const int CanvasSize = 500;
+        private const int CircleDiameter = 50;
+        private const int Frequency = 50;
+        private const int MaxSpeed = 10;
+        private const int MaternitySize = 65;
+        private const int AirPumpSize = 65;
+        #endregion constants
+
+        private int xMax = CanvasSize - CircleDiameter;
+        private int yMax = CanvasSize - CircleDiameter;
+        private HashSet<Molecule> molecules = new HashSet<Molecule>();
+        private Random random = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+            Ellipse ellipse = new Ellipse();
+
+            ellipse.Stroke = SystemColors.WindowFrameBrush;
+            ellipse.Height = 30;
+            ellipse.Width = 50;
+            ellipse.Margin = new Thickness(50);
+            MyCanvas.Children.Add(ellipse);
         }
     }
 }
